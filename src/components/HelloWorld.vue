0007960a6429bec4b7f37b5c48c0fad4d6b74f97
Steven Wang <steven.guan.wang@gmail.com>
2020-10-16 15:38:32 +1000

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,288 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>POC Map</h1>
+    <button v-on:click="stelliteOn">stelliteOn</button>
+    <button v-on:click="stelliteOff">stelliteOff</button>
+    <button v-on:click="FlyToBrisbane">FlyToBrisbane</button>
+    <button v-on:click="EaseToBrisbane">EaseToBrisbane</button>
+    <button v-on:click="JumpToBrisbane">JumpToBrisbane</button>
+    <button v-on:click="JumpToDC">JumpToDC</button>
+    <MglMap
+      :accessToken="accessToken"
+      :mapStyle="mapStyle"
+      @load="onMapLoaded"
+      @click="hideHoverMarker"
+    >
+      <MglNavigationControl position="top-right" />
+      <MglGeolocateControl position="top-right" />
+      <MglScaleControl position="bottom-right" />
+      <MglAttributionControl position="bottom-right" />
+
+      <!-- <MglGeojsonLayer
+        :sourceId="'geojson-source'"
+        :source="geoJsonSource"
+        layerId="myLayera"
+        :layer="geojsonHeatmapLayer"
+      /> -->
+
+      <MglGeojsonLayer
+        :sourceId="'geojson-source'"
+        :source="geoJsonSource"
+        layerId="circle"
+        :layer="geojsonPrettierCircleLayer"
+        @mouseenter="showCircleDetail"
+        @click="circleClick"
+      />
+
+      <MglGeojsonLayer
+        :sourceId="'geojson-source'"
+        :source="geoJsonSource"
+        layerId="text"
+        :layer="geojsonTextLayer"
+      />
+
+      <MglGeojsonLayer
+        :sourceId="'geojson-source'"
+        :source="geoJsonSource"
+        layerId="clusters"
+        :layer="geojsonClusterLayer"
+        @click="clusterClick"
+        @mouseenter="onMouseEnter"
+        @mouseleave="onMouseLeave"
+      />
+
+      <MglGeojsonLayer
+        :sourceId="'geojson-source'"
+        :source="geoJsonSource"
+        layerId="clustersCount"
+        :layer="geojsonClusterCountLayer"
+        @click="clusterClick"
+      />
+
+      <MglMarker
+        :coordinates="[153.03, -27.46]"
+        @mouseenter="
+          showHoverMarker({ content: 'ABC', coordinates: [153.03, -27.46] })
+        "
+      >
+        <h1 style="background-color: white" slot="marker">html marker ABC</h1>
+        <MglPopup>
+          <div>Hello, I'm popup!</div>
+        </MglPopup>
+      </MglMarker>
+
+      <MglMarker
+        :coordinates="[153.04, -27.47]"
+        @mouseenter="
+          showHoverMarker({ content: 'EFG', coordinates: [153.04, -27.47] })
+        "
+      >
+        <h1 style="background-color: white" slot="marker">html marker EFG</h1>
+        <MglPopup>
+          <div>Hello, I'm popup!</div>
+        </MglPopup>
+      </MglMarker>
+
+      <MglMarker
+        v-if="showingHoverMarker"
+        :coordinates="showingHoverMarker.coordinates"
+        :offset="[20, -15]"
+      >
+        <h1 style="background-color: yellow" slot="marker">
+          {{ this.showingHoverMarker.content }}
+        </h1>
+      </MglMarker>
+    </MglMap>
   </div>
 </template>
 
 <script>
+import Mapbox from "mapbox-gl";
+import {
+  MglMap,
+  MglAttributionControl,
+  MglGeolocateControl,
+  MglNavigationControl,
+  MglScaleControl,
+  MglMarker,
+  MglPopup,
+  MglGeojsonLayer,
+} from "vue-mapbox";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  components: {
+    MglMap,
+    MglAttributionControl,
+    MglGeolocateControl,
+    MglNavigationControl,
+    MglScaleControl,
+    MglMarker,
+    MglPopup,
+    MglGeojsonLayer,
+  },
+  props: {},
+  data() {
+    return {
+      accessToken:
+        "ask-ryan-for-token-or-use-your-own", // your access token. Needed if you using Mapbox maps
+      mapStyle: "mapbox://ask-ryan-for-style-or-use-your-own", // your map style
+      showingHoverMarker: null,
+      geoJsonSource: {
+        type: "geojson",
+        data: "/stations.geojson",
+        cluster: true,
+      },
+      geojsonCircleLayer: {
+        type: "circle",
+        source: "geojson-source",
+      },
+      geojsonHeatmapLayer: {
+        type: "heatmap",
+        source: "geojson-source",
+      },
+      geojsonPrettierCircleLayer: {
+        type: "circle",
+        source: "geojson-source",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-color": "#11b4da",
+          "circle-radius": 20,
+          "circle-stroke-width": 5,
+          "circle-stroke-color": "#fff",
+        },
+      },
+      geojsonTextLayer: {
+        type: "symbol",
+        source: "geojson-source",
+        filter: ["!", ["has", "point_count"]],
+        layout: {
+          "icon-image": "white",
+          "icon-text-fit": "both",
+          "icon-text-fit-padding": [1, 5, 1, 5],
+          "text-anchor": "bottom",
+          "text-field": "{title}",
+          "text-font": ["Open Sans Bold"],
+          "text-justify": "center",
+          "text-size": 15,
+          "text-allow-overlap": false,
+          "text-letter-spacing": 0.05,
+          "text-offset": [0, -1.3],
+        },
+        paint: {
+          "text-color": "#ffffff",
+          "text-halo-width": 1,
+          "text-halo-blur": 0,
+          "text-halo-color": "#846cff",
+        },
+      },
+      geojsonClusterLayer: {
+        type: "circle",
+        source: "geojson-source",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1",
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40,
+          ],
+        },
+      },
+      geojsonClusterCountLayer: {
+        id: "cluster-count",
+        type: "symbol",
+        source: "geojson-source",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{point_count_abbreviated}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12,
+        },
+      },
+    };
+  },
+
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
+  },
+  methods: {
+    onMapLoaded(event) {
+      // in component
+      console.log("onMapLoaded");
+      this.map = event.map;
+      this.map.center = [153.04, -27.47];
+
+      this.map.loadImage("/white.png", (error, image) => {
+        if (error) throw error;
+        this.map.addImage("white", image);
+      });
+    },
+    stelliteOn() {
+      this.map.setLayoutProperty("satellite", "visibility", "visible");
+    },
+    stelliteOff() {
+      this.map.setLayoutProperty("satellite", "visibility", "none");
+    },
+    showHoverMarker(obj) {
+      this.showingHoverMarker = obj;
+    },
+    showCircleDetail(event) {
+      const e = event.mapboxEvent;
+      const features = this.map.queryRenderedFeatures(e.point);
+      const [topFeature] = features;
+      console.log(topFeature.geometry.coordinates);
+      this.showingHoverMarker = {
+        coordinates: topFeature.geometry.coordinates,
+        content: topFeature.properties.address,
+      };
+    },
+    hideHoverMarker() {
+      console.log("hideHoverMarker");
+      this.showingHoverMarker = null;
+    },
+    FlyToBrisbane() {
+      this.map.flyTo({ center: [153.03, -27.47], zoom: 13 });
+    },
+    EaseToBrisbane() {
+      this.map.easeTo({ center: [153.03, -27.47], zoom: 13 });
+    },
+    JumpToBrisbane() {
+      this.map.jumpTo({ center: [153.03, -27.47], zoom: 13 });
+    },
+    JumpToDC() {
+      this.map.jumpTo({ center: [-77.0628101291, 38.8846868585], zoom: 13 });
+    },
+    clusterClick(event) {
+      const e = event.mapboxEvent;
+      const zoom = this.map.getZoom();
+      console.log(e.lngLat, zoom);
+      this.map.easeTo({ center: e.lngLat, zoom: zoom + 1 });
+    },
+    circleClick(event) {
+      console.log(event);
+    },
+    onMouseEnter() {
+      this.map.getCanvas().style.cursor = "pointer";
+    },
+    onMouseLeave() {
+      this.map.getCanvas().style.cursor = "";
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +300,11 @@ li {
 }
 a {
   color: #42b983;
+}
+.mgl-map-wrapper {
+  height: 1000px;
+}
+.mapboxgl-popup-content {
+  background-color: yellow;
 }
 </style>
